b3fd260ddb1eebe2b53da5e80459baae638d4e89
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-17 10:01:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
   name_hash.collect do |k, v| 
     if name_hash == ""
       nil 
-    else
-      smallest_v = v < smallest_v
+    elsif v < smallest_v
+      smallest_v = v
       smallest_k = k 
     end 
   end
