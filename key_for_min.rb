8f9888cb496ba0cb8e8ae6261cb95aa2eba7d6f0
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-17 10:02:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,12 +5,26 @@ def key_for_min_value(name_hash)
   smallest_v = nil 
   smallest_k = nil 
   name_hash.each do |k, v| 
-    if name_hash == ""
-      nil 
-    elsif v < smallest_v
+    if v < smallest_v
       smallest_v = v
       smallest_k = k 
     end 
   end
   smallest_k
 end
+
+
+
+
+def key_for_min_value(hash)
+  lowest_key = nil
+  smallest_v = nil
+  hash.each do |k, v|
+    if smallest_v == nil || v < smallest_v
+      smallest_v = v
+      lowest_key = k
+    end
+  end
+  lowest_key
+end
+
